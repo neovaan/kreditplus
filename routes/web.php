78cde242a8ae54183877c452d27c2fd6bc6a2b2d
meggi <meggi@webarq.com>
2017-11-13 08:47:06 +0700

    update route web

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,26 @@
 // Route::get('/', 'About@index');
 Route::get('q','SearchController@index');
 // Route::post('image',['as' => 'upload',function(){
-// 	dd($_FILES)
+//  dd($_FILES)
 
 // }]);
 Route::post('imagex', function(){
-    	if(count($_FILES)){
+        if(count($_FILES)){
             echo $_SERVER['DOCUMENT_ROOT'].'/kreditplus-be/public/ck/';
-    		// if(!is_dir('/ck/'))
-    		// 	mkdir('/ck/');
-    		$d = date('YmdHis');
-    		$basename = basename($_SERVER['DOCUMENT_ROOT']."public/ck/".md5($d)."_".$_FILES['upload']['name']);
-    		move_uploaded_file($_FILES['upload']['tmp_name'],"/home/dev/apps/kreditplus-be/public/ck/".md5($d)."_".$_FILES['upload']['name']);
-    		$funcNum = $_GET['CKEditorFuncNum'] ;
-    		$message = "";
-    		$u = URL::asset('ck/'.md5($d).'_'.$_FILES['upload']['name']);
-    		echo '<script type="text/javascript">window.parent.CKEDITOR.tools.callFunction("'.$funcNum.'", "'.$u.'", "'.$message.'");</script>';
-    		
-    	}
+            // if(!is_dir('/ck/'))
+            //  mkdir('/ck/');
+            $d = date('YmdHis');
+            $basename = basename($_SERVER['DOCUMENT_ROOT']."public/ck/".md5($d)."_".$_FILES['upload']['name']);
+            move_uploaded_file($_FILES['upload']['tmp_name'],"/home/dev/apps/kreditplus-be/public/ck/".md5($d)."_".$_FILES['upload']['name']);
+            $funcNum = $_GET['CKEditorFuncNum'] ;
+            $message = "";
+            $u = URL::asset('ck/'.md5($d).'_'.$_FILES['upload']['name']);
+            echo '<script type="text/javascript">window.parent.CKEDITOR.tools.callFunction("'.$funcNum.'", "'.$u.'", "'.$message.'");</script>';
+            
+        }
     }
 );
 Route::post('newsletter', 'Newsletter@setMail');
+Route::post('kota', 'KotaController@getKota');
+Route::post('setkota', 'KotaController@setKota');
+
