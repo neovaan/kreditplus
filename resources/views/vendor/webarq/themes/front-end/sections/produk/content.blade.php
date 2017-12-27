7d5b68f80b1e95f59a643cce0eb50f1c570f7bb6
meggi <meggi@webarq.com>
2017-12-27 16:14:14 +0700

    update content

--- resources/views/vendor/webarq/themes/front-end/sections/produk/content.blade.php
+++ resources/views/vendor/webarq/themes/front-end/sections/produk/content.blade.php
@@ -3,3 +3,9 @@
    <h3 class="tregular"><?php echo str_replace('"','',$data->intro);?></h3>
    <?php echo $data->description; ?>
 @endforeach
+<script>
+	$('table tbody tr td img').each(function(k,v){
+		console.log($(this).children());
+		$(this).replaceWith("<i class='icwp ic_checked'></i>");
+	});
+</script>
